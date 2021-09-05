b6b0787eeb0e075421b097a8acf544dcd00fafc8
es.halvers <33777515+EunseokHalverson@users.noreply.github.com>
2021-09-05 17:58:48 +0900

    Added SceneDelegate

---
+++ MukMap/Shared/SceneDelegate.swift
@@ -0,0 +1,45 @@
+//
+//  SceneDelegate.swift
+//  MukMap (iOS)
+//
+//  Created by Klown on 2021/09/05.
+//
+
+import Foundation
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+       
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+      
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+       
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+      
+        
+    }
+
+
+}
+
