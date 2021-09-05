557b5612f45ed7ba74fed464b854a8d27bb932e0
es.halvers <33777515+EunseokHalverson@users.noreply.github.com>
2021-09-05 17:16:06 +0900

    Create Xcode Project

---
+++ MukMap/Shared/MukMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MukMapApp.swift
+//  Shared
+//
+//  Created by Klown on 2021/09/05.
+//
+
+import SwiftUI
+
+@main
+struct MukMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
