7f8b2dd94cc7d27f0948cccbd3a251e87cd98646
es.halvers <33777515+EunseokHalverson@users.noreply.github.com>
2021-09-05 17:58:07 +0900

    Created RootTabBarController

---
+++ MukMap/Shared/Controllers/RootTabBarController.swift
@@ -0,0 +1,27 @@
+//
+//  RootTabBarController.swift
+//  MukMap (iOS)
+//
+//  Created by Klown on 2021/09/05.
+//
+
+import Foundation
+import UIKit
+
+class RootTabBarController: UITabBarController {
+    
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(false)
+    }
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewWillDisappear(false)
+    }
+    
+}
